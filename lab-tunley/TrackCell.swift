f13d9498dae4d70f7c6f555887aacf8d4833958a
allyomega <wdonahue2012@fau.edu>
2023-09-06 18:37:24 -0400

    Created and configured a custom cell class

---
+++ lab-tunley/TrackCell.swift
@@ -0,0 +1,35 @@
+//
+//  TrackCell.swift
+//  lab-tunley
+//
+//  Created by William Donahue on 9/6/23.
+//
+
+import UIKit
+import Nuke
+
+
+class TrackCell: UITableViewCell {
+
+    @IBOutlet weak var trackImageView: UIImageView!
+  
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var trackArtistLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        func configure(with track: Track) {
+            trackNameLabel.text = track.trackName
+            trackArtistLabel.text = track.artistName
+            
+            Nuke.loadImage(with: (track.artworkUrl100), into: trackImageView)
+        }
+    }
+
+}
